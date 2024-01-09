0190403d39ca09fa88e549dddd9dd1b8b3009dbb
Ahmed Bilal <ahmed.bilal@t-online.de>
2024-01-09 19:30:36 +0100

    Strapi Submenus
    Create four components: Navbar, Hero, Sidebar, and Submenu.

---
+++ README.md
@@ -0,0 +1,8 @@
+Strapi Submenus
+
+create strapi like submenus
+
+part of learn react series
+
+Create four components: Navbar, Hero, Sidebar, and Submenu.
+
---
+++ src/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+
+    return <div className="hero-container">
+        <div className="hero-center">
+            <h1>Set up a Hero component <br></br>and add CSS.</h1>
+            <p>
+                Set up NavLinks in the Navbar. Import subunits from data and iterate over them to render pages on the screen. Set up a NavLink CSS.
+            </p>
+        </div>
+    </div>;
+};
+
+export default Hero;
